2960849d0db0747a9990c0b89ebb9c1e80443180
roldanronaldd19-debug <roldanronaldd19@gmail.com>
2026-01-09 20:21:28 -0500

    Create ToastProvider.tsx

---
+++ components/ToastProvider.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Toaster } from 'react-hot-toast'
+
+export default function ToastProvider() {
+  return (
+    <Toaster
+      position="top-right"
+      gutter={8}
+      toastOptions={{
+        duration: 4000,
+        style: {
+          background: '#363636',
+          color: '#fff',
+          borderRadius: '12px',
+          padding: '16px',
+          fontSize: '14px',
+          maxWidth: '500px',
+        },
+        success: {
+          duration: 3000,
+          iconTheme: {
+            primary: '#10B981',
+            secondary: '#fff',
+          },
+        },
+        error: {
+          duration: 4000,
+          iconTheme: {
+            primary: '#EF4444',
+            secondary: '#fff',
+          },
+        },
+      }}
+    />
+  )
+}
